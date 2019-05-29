6a2fe285c2dc56862d55c003d9c581657993ea2b
coderhook <abel1987@hotmail.es>
2019-05-29 20:34:25 +0200

    Create Element with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -13,4 +13,34 @@ their name and comment will be displayed on the page. */
 
 function submitComment() {
     console.log('Hello');
+    const inputField = document.querySelector('#name');
+    const textArea = document.querySelector('#msg');
+    const name = inputField.value;
+    const msg = textArea.value;
+    console.log(name + ': ' + msg);
+
+    insertComment(name, msg);
+    
+}
+
+function insertComment(name, msg) {
+    console.log('Im in insertComment')
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    console.log(comment);
+    console.log(h3);
+    console.log(p);
+
+    
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = `${msg}`;
+
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    console.log(comment);
+
 }
