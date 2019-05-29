0f9a344896d80398f5d44443cff95d72bab07bf2
coderhook <abel1987@hotmail.es>
2019-05-29 23:29:48 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -17,30 +17,30 @@ function submitComment() {
     const textArea = document.querySelector('#msg');
     const name = inputField.value;
     const msg = textArea.value;
-    console.log(name + ': ' + msg);
 
+    //Call the function to add comments.
     insertComment(name, msg);
+
+    //Reset values
+    inputField.value = null;
+    textArea.value = null;
     
 }
 
+//Inserting comments.
 function insertComment(name, msg) {
-    console.log('Im in insertComment')
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
-
-    console.log(comment);
-    console.log(h3);
-    console.log(p);
-
     
     h3.innerHTML = `${name} said:`;
-    p.innerHTML = `${msg}`;
+    p.innerHTML = ` ${msg}`;
 
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
 
-    console.log(comment);
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
 
 }
